249f7c7a0e5e2b2626cf65e036c247f6500a4612
somename <someemail>
2017-05-12 20:10:12 +0200

    besouro automatic message

--- src/Pozicija.java
+++ src/Pozicija.java
@@ -2,11 +2,32 @@
 public class Pozicija {
 	private int x;
 	private int y;
+	private String smer;
 	
-	public Pozicija(int x, int y, String direction){
+	public Pozicija(int x, int y, String smer){
 		this.x = x;
 		this.y = y;
+		smer = "N";
 	}
 	
+	public String trenutnaPozicija(){
+		return "(" + x + ", " + y + ", " + smer + ")"; 
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 	
 }
