8a72ecc491b407d0f54436a11654b7826c12936d
somename <someemail>
2017-05-12 20:26:08 +0200

    besouro automatic message

--- src/PlanetExplorerException.java
+++ src/PlanetExplorerException.java
@@ -1,4 +1,10 @@
 
 public class PlanetExplorerException extends Exception {
-
+	public PlanetExplorerException(){
+		
+	}
+	
+	public PlanetExplorerException(String poruka){
+		super(poruka);
+	}
 }
