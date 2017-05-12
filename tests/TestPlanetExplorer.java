ef8d6ca9990b377d2e587a929963edd5c7bfb143
somename <someemail>
2017-05-12 20:13:53 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -5,7 +5,12 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	@Test
-	public void test_executeCommand() {
-		fail("Not yet implemented");
+	public void test_pocetnaPozicija() {
+		//Arrenge
+		Pozicija poz = new Pozicija(0,0,"N");
+		//Act
+		String izlaz = poz.toString();
+		//Assert
+		assertEquals("Provera startne pozicije", izlaz, "(0, 0, N");
 	}
 }
